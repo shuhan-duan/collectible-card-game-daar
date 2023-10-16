09c3efe7110ae96c0babc66c3aea3e6c58ea0889
shuhan-duan <shuhanduan99@gmail.com>
2023-10-16 12:21:37 +0200

    contracts

--- contracts/src/Collection.sol
+++ contracts/src/Collection.sol
@@ -1,12 +1,47 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8;
+pragma solidity ^0.8.7 .0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Collection {
   string public name;
   int public cardCount;
 
-  constructor(string memory _name, int _cardCount) {
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  struct Card {
+    string name;
+    int cardCount;
+  }
+
+  mapping(uint => Card) public cards;
+
+  constructor(
+    string memory _name,
+    uint _cardCount
+  ) ERC721(_name, "TOKEN_SYMBOL_HERE") {
     name = _name;
     cardCount = _cardCount;
+  }
+
+  // creat cards for myself
+  function addCard(string memory img) external {
+    _tokenIds.increment();
+    uint256 newCardId = _tokenIds.current();
+    _mint(msg.sender, newCardId); // Mint a new NFT to the caller
+
+    Card memory newCard = Card({cardNumber: newCardId, img: img});
+    cards[newCardId] = newCard;
+  }
+
+  function mintTo(address to, string memory img) external {
+    _tokenIds.increment();
+    uint256 newCardId = _tokenIds.current();
+    _mint(to, newCardId);
+
+    Card memory newCard = Card({cardNumber: newCardId, img: img});
+    cards[newCardId] = newCard;
   }
 }
--- contracts/src/Main.sol
+++ contracts/src/Main.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8;
+pragma solidity ^0.8.7 .0;
 
 import "./Collection.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Main {
+contract Main is Ownable {
   int private count;
   mapping(int => Collection) private collections;
 
@@ -11,7 +12,23 @@ contract Main {
     count = 0;
   }
 
-  function createCollection(string calldata name, int cardCount) external {
-    collections[count++] = new Collection(name, cardCount);
+  function createCollection(
+    string memory name,
+    uint cardCount
+  ) external onlyOwner {
+    Collection newCollection = new Collection(name, cardCount);
+    collections[count++] = newCollection;
+  }
+
+  function mintCardToCollection(
+    uint collectionId,
+    address to,
+    string memory img
+  ) external onlyOwner {
+    require(
+      collections[collectionId] != Collection(address(0)),
+      "Collection does not exist"
+    );
+    collections[collectionId].mintTo(to, img);
   }
 }
